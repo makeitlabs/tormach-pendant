ccaf6c01b1610d2414efb780f517aaad6d36c64f
Steve Richardson <steve.richardson@makeitlabs.com>
2023-02-26 19:29:11 -0500

    work in progress for real hardware, 3 encoders working on Teensy 3.5

--- teensy/tormach_controls/tormach_controls.ino
+++ teensy/tormach_controls/tormach_controls.ino
@@ -1,6 +1,6 @@
 // work-in-progress R&D for new Tormach Operator Console emulation mode
 //
-// 2023-FEB-18 Steve Richardson (steve.richardson@makeitlabs.com)
+// 2023-FEB-26 Steve Richardson (steve.richardson@makeitlabs.com)
 //
 
 #define ENCODER_USE_INTERRUPTS
@@ -8,91 +8,216 @@
 
 #include <Encoder.h>
 #include <Bounce.h>
+#include <FastLED.h>
 
-#define PIN_BEACON_BLUE 0
-#define PIN_BEACON_GREEN 1
-#define PIN_BEACON_AMBER 2
-#define PIN_BEACON_RED 3
+#define PIN_BTN_V 0
+#define PIN_ENCODER_V_A 1
+#define PIN_ENCODER_V_B 2
 
-#define PIN_BTN_START 4
-#define PIN_BTN_STOP 6
-#define PIN_BTN_FEED 7
-#define PIN_BTN_STATUS 8
+#define PIN_BTN_F 3
+#define PIN_ENCODER_F_A 4
+#define PIN_ENCODER_F_B 5
 
-#define PIN_BTN_FOV 10
-#define PIN_BTN_SOV 18
-
-#define PIN_ENCODER_A 11
-#define PIN_ENCODER_B 12
+#define PIN_BTN_S 6
+#define PIN_ENCODER_S_A 7
+#define PIN_ENCODER_S_B 8
 
 #define PIN_JOG_A 14
 #define PIN_JOG_B 15
 
-#define PIN_LED_START 5
-#define PIN_LED_FEED 16
-#define PIN_LED_STATUS 17
-#define PIN_LED_RFID 21
 
-Encoder encoder(PIN_ENCODER_A, PIN_ENCODER_B);
-Encoder jog(PIN_JOG_A, PIN_JOG_B);
+#define NUM_RGB_LEDS 12
+#define PIN_RGB_LED_DOUT 32
 
-Bounce button_start = Bounce(PIN_BTN_START, 10);
-Bounce button_stop = Bounce(PIN_BTN_STOP, 10);
-Bounce button_feed = Bounce(PIN_BTN_FEED, 10);
-Bounce button_status = Bounce(PIN_BTN_STATUS, 10);
-Bounce button_fov = Bounce(PIN_BTN_FOV, 10);
-Bounce button_sov = Bounce(PIN_BTN_SOV, 10);
 
-static bool b_start = false, b_stop = false, b_feed = false, b_status = false, b_fov = false, b_sov = false;
+CRGB rgb_leds[NUM_RGB_LEDS];
 
-static uint16_t enc_vals[3] = { 999, 500, 500 };
-static uint8_t enc_val_idx = 0;
+Encoder enc_vel(PIN_ENCODER_V_A, PIN_ENCODER_V_B);
+Encoder enc_feed(PIN_ENCODER_F_A, PIN_ENCODER_F_B);
+Encoder enc_speed(PIN_ENCODER_S_A, PIN_ENCODER_S_B);
 
-#define ENC_IDX_VOV 0
-#define ENC_IDX_SOV 1
-#define ENC_IDX_FOV 2
+Bounce button_vel = Bounce(PIN_BTN_V, 10);
+Bounce button_feed = Bounce(PIN_BTN_F, 10);
+Bounce button_speed = Bounce(PIN_BTN_S, 10);
+
+
+
+#define ENC_VEL_MAX 31
+#define ENC_VEL_INIT 0
+#define ENC_VEL_DEFAULT 0
+
+#define ENC_FEED_MAX 200
+#define ENC_FEED_INIT 100
+#define ENC_FEED_DEFAULT 100
+
+#define ENC_SPEED_MAX 200
+#define ENC_SPEED_INIT 100
+#define ENC_SPEED_DEFAULT 100
+
 
 void setup() 
 {
- 
-  pinMode(PIN_BTN_START, INPUT_PULLUP);
-  pinMode(PIN_BTN_STOP, INPUT_PULLUP);
-  pinMode(PIN_BTN_FEED, INPUT_PULLUP);
-  pinMode(PIN_BTN_STATUS, INPUT_PULLUP);
-  pinMode(PIN_BTN_FOV, INPUT_PULLUP);
-  pinMode(PIN_BTN_SOV, INPUT_PULLUP);
-
-  //pinMode(PIN_ENCODER_A, INPUT_PULLUP);
-  //pinMode(PIN_ENCODER_B, INPUT_PULLUP);
-  //pinMode(PIN_JOG_A, INPUT_PULLUP);
-  //pinMode(PIN_JOG_B, INPUT_PULLUP);
-  
-  pinMode(PIN_LED_START, OUTPUT);
-  pinMode(PIN_LED_FEED, OUTPUT);
-  pinMode(PIN_LED_STATUS, OUTPUT);
-  pinMode(PIN_LED_RFID, OUTPUT);
-  pinMode(PIN_BEACON_GREEN, OUTPUT);
-  pinMode(PIN_BEACON_AMBER, OUTPUT);
-  pinMode(PIN_BEACON_RED, OUTPUT);
-  pinMode(PIN_BEACON_BLUE, OUTPUT);
-
-  digitalWrite(PIN_LED_START, 1);
-  digitalWrite(PIN_LED_FEED, 1);
-  digitalWrite(PIN_LED_STATUS, 1);
-  digitalWrite(PIN_LED_RFID, 1);
-  
-  digitalWrite(PIN_BEACON_GREEN, 1);
-  digitalWrite(PIN_BEACON_AMBER, 1);
-  digitalWrite(PIN_BEACON_RED, 1);
-  digitalWrite(PIN_BEACON_BLUE, 1);
+  pinMode(PIN_BTN_V, INPUT_PULLUP);
+  pinMode(PIN_BTN_F, INPUT_PULLUP);
+  pinMode(PIN_BTN_S, INPUT_PULLUP);
 
   Tormach.begin();
   Tormach.useManualSend(true);
 
-  encoder.write(500);
-  jog.write(1);
+  enc_vel.write(ENC_VEL_INIT);
+  enc_feed.write(ENC_FEED_INIT);
+  enc_speed.write(ENC_SPEED_INIT);
+
 }
 
+bool encoder_limit(int &inval, int maxval)
+{
+  if (inval < 0) {
+    inval = 0;
+    return true;
+  } else if (inval > maxval) {
+    inval = maxval;
+    return true;
+  }
+  return false;
+}
+
+bool encoder_poll()
+{ 
+  int cur_vel=0, cur_feed=0, cur_speed=0;
+  static int last_vel=-1, last_feed=-1, last_speed=-1;
+  bool updated = false;
+  
+  static elapsedMillis elapsed_vel_pressed;
+
+  const int vel_lut[] = {10,20,31,41,51,61,72,82,92,102,113,123,133,153,205,256,307,358,409,460,512,563,614,665,716,767,818,870,921,972,1023};
+  
+  // check for encoder shaft button press, which resets values to default
+  button_vel.update();
+  if (button_vel.fallingEdge()) {
+    elapsed_vel_pressed=0;
+  } else if (button_vel.risingEdge()) {
+    if (elapsed_vel_pressed >= 500) {
+      enc_vel.write(ENC_VEL_MAX);
+    } else if (elapsed_vel_pressed > 100 && elapsed_vel_pressed < 500) {
+      enc_vel.write(ENC_VEL_DEFAULT);
+    }
+  }
+
+
+  button_feed.update();
+  if (button_feed.fallingEdge()) {
+    enc_feed.write(ENC_FEED_DEFAULT);
+  }
+
+  button_speed.update();
+  if (button_speed.fallingEdge()) {
+    enc_speed.write(ENC_SPEED_DEFAULT);
+  }
+
+  cur_vel = enc_vel.read();
+  if (encoder_limit(cur_vel, ENC_VEL_MAX)) enc_vel.write(cur_vel);
+
+  cur_feed = enc_feed.read();
+  if (encoder_limit(cur_feed, ENC_FEED_MAX)) enc_feed.write(cur_feed);
+
+  cur_speed = enc_speed.read();
+  if (encoder_limit(cur_speed, ENC_SPEED_MAX)) enc_speed.write(cur_speed);
+
+  if (cur_vel != last_vel || cur_feed != last_feed || cur_speed != last_speed) {
+    last_vel = cur_vel;
+    last_feed = cur_feed;
+    last_speed = cur_speed;
+    updated=true;
+    //Serial.println(String("enc: V=") + cur_vel + String(" F=") + cur_feed + String(" S=") + cur_speed);
+  }
+ 
+  // 0 - ABS_X - feed-override - "F%" confirmed
+  // 1 - ABS_Y - rpm-override - "S%" confirmed
+  // 2 - ABS_Z - rapid-override - "V%" confirmed (old "maxvel" equivalent)
+  // 3 - ABS_RX - axis-select - no response
+  // 4 - ABS_RY - step-select - no response
+  // 5 - ABS_MISC - pendant-knob
+  // 6 - ABS_WHEEL - jog-wheel
+  // 7 - ABS_GAS - jog-wheel-2
+
+  Tormach.absolute(0, (cur_feed * 1023) / ENC_FEED_MAX);
+  Tormach.absolute(1, (cur_speed * 1023) / ENC_SPEED_MAX);
+  Tormach.absolute(2, vel_lut[cur_vel]);
+
+  Tormach.absolute(3, 1); // X axis
+  Tormach.absolute(4, 1); // 0.001
+  Tormach.absolute(5, 2); // knob=2
+  Tormach.absolute(6, 0); // jog
+  Tormach.absolute(7, 0); // jog-2
+  
+  return updated;
+}
+
+void loop() 
+{
+
+  //led_poll();
+  
+  //bool button_update = button_poll();
+  bool encoder_update = encoder_poll();
+  
+  //rgb_poll();
+
+  if (encoder_update) {
+    Tormach.send_now();
+  }
+
+}
+
+
+
+
+  /*
+  FastLED.addLeds<WS2812, PIN_RGB_LED_DOUT, GRB>(rgb_leds, NUM_RGB_LEDS);
+
+  FastLED.setBrightness(255);
+  for (int i=0; i< NUM_RGB_LEDS; i++) {
+    rgb_leds[i] = CRGB::Black;
+  }
+  */
+
+
+/*
+void rgb_poll()
+{
+  static int value = 0;
+  static bool flip = false;
+  
+  if (tic >= 200) {
+
+    for (int i=0; i < NUM_RGB_LEDS; i++) {
+      if (i < value) rgb_leds[i] = CRGB::Blue;
+      else if (i == value) rgb_leds[i] = CRGB::White;
+      else rgb_leds[i] = CRGB::Black;
+    }
+
+    FastLED.show();
+    
+    tic = 0;
+
+    if (flip) {
+      value--;
+      if (value == 0) {
+        flip = !flip;      
+      }
+    } else {
+      value++;
+      if (value == 11) {
+        flip = !flip;
+      }
+    }
+  }
+}
+*/
+
+ 
+/*
 bool led_poll()
 {
   int r = Tormach.update();
@@ -116,7 +241,9 @@ bool led_poll()
   }
   return false;
 }
+*/
 
+/*
 bool button_poll() 
 {
   uint8_t u = 0, uov = 0;
@@ -133,20 +260,12 @@ bool button_poll()
   if (button_fov.fallingEdge()) { b_fov = 1; uov = 1;} else if (button_fov.risingEdge()) { b_fov = 0; uov = 1;}
   if (button_sov.fallingEdge()) { b_sov = 1; uov = 1;} else if (button_sov.risingEdge()) { b_sov = 0; uov = 1;}
 
-/*
- * Additional possible functions via keyboard shortcuts:
- * 
- * F1 = peek at status tab
- * 
- * Esc or Alt-S = stop
- * 
- * Alt+Enter = MDI line focus
- * 
- * Alt+F = toggle Flood
- * Alt+M = toggle Mist
- * 
- * 
- */
+ // Additional possible functions via keyboard shortcuts:
+ // F1 = peek at status tab
+ // Esc or Alt-S = stop
+ // Alt+Enter = MDI line focus
+ // Alt+F = toggle Flood
+ // Alt+M = toggle Mist
 
   // STOP is sent via emulated keyboard - ESC key
   if (button_stop.fallingEdge()) {
@@ -199,80 +318,4 @@ bool button_poll()
     
   return u==1;
 }
-
-bool encoder_poll()
-{ 
-  static uint8_t last_enc_val_idx = 255;
-  
-  static int16_t last_jog_val = -1;
-  int16_t enc_val, jog_val;
-  
-  bool updated = false;
-  
-  if (enc_val_idx != last_enc_val_idx) {
-    encoder.write(enc_vals[enc_val_idx]);
-    //Serial.println(String("enc_val_idx: ") + enc_val_idx);
-  }
-  last_enc_val_idx = enc_val_idx;
-
-  // read the encoder value, limit it to 0-999, save it
-  enc_val = encoder.read();
-  if (enc_val < 0) {
-    enc_val = 0;
-  } else if (enc_val > 999) {
-    enc_val = 999;
-  }
-
-  if (enc_vals[enc_val_idx] != enc_val) {
-    enc_vals[enc_val_idx] = enc_val;
-    updated = true;
-    //Serial.println(String("enc: ") + enc_val);
-  }
-
-
-  jog_val = jog.read();
-  
-  if (last_jog_val != jog_val) {
-    last_jog_val = jog_val;
-    updated = true;
-    //Serial.println(String("jog: ") + jog_val);
-  }
-
-
-  // 0 - ABS_X - feed-override - "F%" confirmed
-  // 1 - ABS_Y - rpm-override - "S%" confirmed
-  // 2 - ABS_Z - rapid-override - "V%" confirmed (old "maxvel" equivalent)
-  // 3 - ABS_RX - axis-select - no response
-  // 4 - ABS_RY - step-select - no response
-  // 5 - ABS_MISC - pendant-knob
-  // 6 - ABS_WHEEL - jog-wheel
-  // 7 - ABS_GAS - jog-wheel-2
-
-  Tormach.absolute(0, enc_vals[ENC_IDX_FOV]);
-  Tormach.absolute(1, enc_vals[ENC_IDX_SOV]);
-  Tormach.absolute(2, enc_vals[ENC_IDX_VOV]);
-
-  Tormach.absolute(3, 1); // X axis
-  Tormach.absolute(4, 1); // 0.001
-  Tormach.absolute(5, 2); // knob=2
-  Tormach.absolute(6, jog_val);
-  Tormach.absolute(7, 0); // jog-2
-  
-  return updated;
-}
-
-void loop() 
-{
-
-  led_poll();
-  
-  bool button_update = button_poll();
-  bool encoder_update = encoder_poll();
-  
-  if (button_update || encoder_update) {
-    Tormach.send_now();
-  }
-
-}
-
- 
+*/
